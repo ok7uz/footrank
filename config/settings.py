6724021fea7d8d346014cedd3ccfad551e5a32a5
ok7uz <komronbekobloev@gmail.com>
2024-10-16 15:20:13 +0500

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-tk!*pmh$#rj3)wv7#2@(f*3ij!^dw=+^q@(@(#^7b3!k-n=748
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
